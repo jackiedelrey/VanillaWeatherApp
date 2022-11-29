827a2239d6fca09dab808f755a7b2a063c49b1bd
Jackie <jackieshields01@gmail.com>
2022-11-29 19:38:31 +0000

    logged API results with app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 function displayTemperature(response) {
-  console.log(response.data.temperature.current);
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#current-temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "777of6aa4d00083100225cb6ff7437te";
