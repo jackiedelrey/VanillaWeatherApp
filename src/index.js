3df9319236705d429bcfbcfe9037fbcb8120c51e
Jackie <jackieshields01@gmail.com>
2022-12-08 11:26:00 +0000

    setting up forecast JS code

--- src/index.js
+++ src/index.js
@@ -21,6 +21,28 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = `
+                       <div class="row">
+                          <div class="col-2">
+                            <div class="weather-forecast-date">Thu</div>
+                            <img
+                              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-night.png"
+                              alt=""
+                              width="42"
+                            />
+                            <div class="weather-forecast-temperatures">
+                              <span class="weather-forecast-temperature-max">
+                                18°</span
+                              >
+                              <span class="weather-forecast-temperature-min">
+                                12°
+                              </span>
+                            </div>
+                          </div>`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let cityElement = document.querySelector("#city");
@@ -79,3 +101,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Glasgow");
+displayForecast();
