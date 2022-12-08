12f8a27721a8656986a3da4edd8348661a68f0fe
Jackie <jackieshields01@gmail.com>
2022-12-08 12:31:17 +0000

    updated JS with forecasting from html

--- src/index.js
+++ src/index.js
@@ -23,24 +23,32 @@ function formatDate(timestamp) {
 
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = `
-                       <div class="row">
-                          <div class="col-2">
-                            <div class="weather-forecast-date">Thu</div>
-                            <img
-                              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-night.png"
-                              alt=""
-                              width="42"
-                            />
-                            <div class="weather-forecast-temperatures">
-                              <span class="weather-forecast-temperature-max">
-                                18°</span
-                              >
-                              <span class="weather-forecast-temperature-min">
-                                12°
-                              </span>
-                            </div>
-                          </div>`;
+
+  let forecastHTML = `<div class=row>`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-night.png"
+                alt=""
+                width="42"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">
+                  18°</span
+                >
+                <span class="weather-forecast-temperature-min">
+                  12°
+                </span>
+              </div> </div>
+            `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
